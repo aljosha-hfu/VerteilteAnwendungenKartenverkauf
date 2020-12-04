43f5e5e4ffa1f9f8b1cacef1f75cfded051f1842
Aljosha <aljoshajustin@gmail.com>
2020-12-04 12:03:57 +0100

    added test for ticket cancellation

---
+++ src/test/java/de/aljoshavieth/kartenverkauf/KartenverkaufTest.java
@@ -0,0 +1,42 @@
+package de.aljoshavieth.kartenverkauf;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+class KartenverkaufTest {
+
+    Kartenverkauf kartenverkauf = new Kartenverkauf();
+    final Logger logger = Logger.getLogger("KartenverkaufTest");
+
+    @BeforeEach
+    void setUp() {
+        logger.info("Setting up Kartenverkauf");
+        kartenverkauf = new Kartenverkauf();
+        for (int i = 0; i < 100; i++) {
+            Ticket ticket = new Ticket();
+            if (i % 2 == 0) {
+                ticket.setState(TicketState.RESERVED);
+            } else {
+                ticket.setState(TicketState.AVAILABLE);
+            }
+            kartenverkauf.tickets[i] = ticket;
+        }
+        logger.info("Printing all TicketStates:");
+        Arrays.stream(kartenverkauf.tickets).forEach(t -> System.out.println(t.getState()));
+    }
+
+
+    @Test
+    void cancelAllTicketReservations() {
+        logger.info("Cancelling all ticket reservations");
+        kartenverkauf.cancelAllTicketReservations();
+        logger.info("Printing all TicketStates:");
+        Arrays.stream(kartenverkauf.tickets).forEach(t -> System.out.println(t.getState()));
+        assumeTrue(Arrays.stream(kartenverkauf.tickets).allMatch(ticket -> ticket.getState() == TicketState.AVAILABLE));
+    }
+}
